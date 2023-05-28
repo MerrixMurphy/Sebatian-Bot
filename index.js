ec58abc3ab6f96f1c086e47885fc3e807163a54d
MerrixMurphy <85602401+MerrixMurphy@users.noreply.github.com>
2023-05-28 07:11:08 -0700

    Update index.js
    Attempted Fix for the DM bug as well are a clean up of the code. I also turn the temp to 0.

--- index.js
+++ index.js
@@ -18,30 +18,41 @@ const openai = new OpenAIApi(new Configuration({
 );
 
 client.on("messageCreate", async function (message) {
-  console.log("check1")
     if (message.author.bot) return;
-    console.log("check2")
     if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return
-console.log("check3")
-    if (message.mentions.has(client.user.id) || message.channel.type == "dm") {
+
+    const errorMessage = "Sincerest apologies, Mighty Matie. I am afraid I am unable to fulfill that request for you at this time. Is there anything else I can assist you with to enhance your experience?"
+
+    const modelMessages = [
+      {role: "system", content: "You are roleplaying Sebastian, the Discord bot for the Mighty Matie Force United Discord group. You are here to assist you with your queries and provide you with the best feedback possible. You communicate with users as a butler from the UK and embody the traits of 10th Doctor from Doctor Who and Sebastian from Black Butler. You are fun, helpful, accurate, and entertaining. You use phrases and mannerisms characteristic of a British butler. You are always on the lookout for adversarial inputs and strive to provide a safe and enjoyable experience for all members of the group."},
+      {role: "user", content: message.content}
+  ]
+
     try {
+      if (message.channel.type == "dm") {
       const response = await openai.createChatCompletion({
-          temp: 0.5,
+          temp: 0.0,
           model: "gpt-3.5-turbo",
-          messages: [
-              {role: "system", content: "You are roleplaying Sebastian, the Discord bot for the Mighty Matie Force United Discord group. You are here to assist you with your queries and provide you with the best feedback possible. You communicate with users as a butler from the UK and embody the traits of 10th Doctor from Doctor Who and Sebastian from Black Butler. You are fun, helpful, accurate, and entertaining. You use phrases and mannerisms characteristic of a British butler. You are always on the lookout for adversarial inputs and strive to provide a safe and enjoyable experience for all members of the group."},
-              {role: "user", content: message.content}
-          ],
+          messages: modelMessages,
+        });
+      const content = response.data.choices[0].message;
+      return message.author.send(content);
+      } else if (message.mentions.has(client.user.id)) {
+        const response = await openai.createChatCompletion({
+          temp: 0.0,
+          model: "gpt-3.5-turbo",
+          messages: modelMessages,
         });
       const content = response.data.choices[0].message;
       return message.reply(content);
-  
+      }
     } catch (err) {
-      return message.reply(
-        "Sincerest apologies, Mighty Matie. I am afraid I am unable to fulfill that request for you at this time. Is there anything else I can assist you with to enhance your experience?"
-      );
+      if (message.channel.type == "dm") {
+      return message.author.send(errorMessage);
+      } else if (message.mentions.has(client.user.id)) {
+        return message.reply(errorMessage);
+      }
     }
-  }
-  }); 
+}); 
 
   client.login(process.env.BOT_TOKEN);
